b5eeb9e92c006ee4a5f605ff3814de2895a81605
Naga Maheswara <nagamaheshwara1997@gmail.com>
2018-08-01 16:33:46 +0530

    removed unwanted stuff

--- api.py
+++ api.py
@@ -31,12 +31,8 @@ def make_prediction():
 	if request.method=='POST':
 		path = '/home/sreemahavishnu/Desktop/programming/courses/udacity/deep_learning/facial_recognition/image1.jpg'
 		img = face_recognition.load_image_file(path)
-		#print(img)
-		#img = img.reshape(1, -1)
 		X_face_locations = face_recognition.face_locations(img)
-		#print(X_face_locations)
 		faces_encodings = face_recognition.face_encodings(img, known_face_locations=X_face_locations)
-		#print(faces_encodings)
 		closest_distances = model.kneighbors(faces_encodings, n_neighbors=1)
 		are_matches = [closest_distances[0][i][0] <= 0.6 for i in range(len(X_face_locations))]
 		prediction = [pred if rec else ("unknown", loc) for pred, loc, rec in zip(model.predict(faces_encodings), X_face_locations, are_matches)]
@@ -47,35 +43,13 @@ def take_picture(port = 0):
 	if request.method == 'POST' :
 		camera = cv2.VideoCapture(0)
 		print(camera.isOpened())
-		#camera.set(3, 1280)
-		#camera.set(4, 720)
 		for i in range(0, 30):
 			temp = camera.read()
 		retval, im = camera.read()
 		cv2.imwrite('image1.jpg',im)
 		camera.release()
 		del(camera)
-		'''camera_port = 0
-		ramp_frames = 30
-		camera = cv2.VideoCapture(0)
-		retval, im = camera.read()
-		for i in range(0, ramp_frames):
-			temp = camera.read()
-		filename = "aj.jpg"
-		cv2.imwrite(filename,im)
-		del(camera)'''
 		return render_template('index.html', label = "picture captured")
-@app.route('/predict_capture_pygame', methods = ['POST'])
-def predict_capture_pygame() :
-	if request.method == 'POST' :
-		pygame.camera.init()
-		camera = pygame.camera.Camera('/dev/video0', (800, 600))
-		camera.start()
-		for i in range(0, 30) :
-			temp = camera.get_image()
-		img = camera.get_image()
-		pygame.image.save(img, 'image1.jpg')
-		camera.stop()
 @app.route('/train_snap', methods = ['POST'])
 def train_capture() :
 	if request.method == 'POST' :
@@ -89,27 +63,7 @@ def train_capture() :
 		cv2.imwrite('photos_temp/image1.jpg',im)
 		camera.release()
 		del(camera)
-		'''camera_port = -1
-		ramp_frames = 30
-		camera = cv2.VideoCapture(0)
-		retval, im = camera.read()
-		for i in range(0, ramp_frames):
-			temp = camera.read()
-		filename = "aj.jpg"
-		cv2.imwrite(filename,im)
-		del(camera)'''
 		return render_template('index.html', label = "picture captured")
-@app.route('/train_capture_pygame', methods = ['POST'])
-def train_capture_pygame() :
-	if request.method == 'POST' :
-		pygame.camera.init()
-		camera = pygame.camera.Camera('/dev/video0', (800, 600))
-		camera.start()
-		for i in range(0, 30) :
-			temp = camera.get_image()
-		img = camera.get_image()
-		pygame.image.save(img, 'photos_temp/image1.jpg')
-		camera.stop()
 @app.route('/train', methods = ['POST'])
 def train_image() :
 	if request.method == 'POST' :
@@ -143,7 +97,6 @@ def train(train_dir='/home/sreemahavishnu/Desktop/programming/courses/udacity/de
         n_neighbors = int(round(math.sqrt(len(X))))
         if verbose:
             print("Chose n_neighbors automatically:", n_neighbors)
-    #print(X.shape())
     knn_clf = neighbors.KNeighborsClassifier(n_neighbors=n_neighbors, algorithm=knn_algo, weights='distance')
     knn_clf.fit(X, y)
 
